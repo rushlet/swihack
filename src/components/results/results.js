3814a5cc71b531678fb9e8924b68afb2a5a2cc02
Becky Rush <becky.rush@bbc.co.uk>
2020-02-22 09:14:39 +0100

    start adding results

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,11 +1,23 @@
+import data from '../../measures_swihack';
+
 export default class Results {
     constructor() {
         this.resultContainer = document.querySelector('.results-container');
     }
 
-    renderResults(data) {
-        this.resultContainer.innerHTML = `
-            <h2>Results for ${data.type}, ${data.unit}, ${data.value}</h2>
-        `;
+    renderResults(options) {
+        this.suggestions = this.getExamplesForSelection(options);
+        const markup = this.suggestions.reduce((previous, current, i) => {
+            console.log('current', this.examplesForType[current]);
+            return `${previous}<img class="${current}" src="${this.examplesForType[current].icon}">`
+        }, `<h2>Results for ${options.type}, ${options.unit}, ${options.value}</h2><div class="examples-container">`);
+        console.log('markup', markup);
+        this.resultContainer.innerHTML = `${markup}</div>`;
+    }
+
+    getExamplesForSelection(options) {
+        const selectedUnit = options.unit;
+        this.examplesForType = data.type[options.type].examples;
+        return Object.keys(this.examplesForType).filter(key => this.examplesForType[key].unit.includes(selectedUnit));
     }
 }
